69282d8c2c7890ec6724fbe45ad1954d61f803ea
Omar Abouraia <omara862005@gmail.com>
2025-11-26 02:51:25 +0200

    Updating routes in app.jsx

--- client/reddit/src/App.jsx
+++ client/reddit/src/App.jsx
@@ -1,28 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { useState } from 'react'
-import './App.css'
-<<<<<<< Updated upstream
-import CreationWizard from '../components/CommunityCreation/CreationWizard'
-=======
-import CommunityDetails from '../components/CommunityCreation/CommunityDetails/CommunityDetails'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import CreationWizard from '../components/CommunityCreation/CreationWizard';
+import CommunityDetails from '../components/CommunityCreation/CommunityDetails/CommunityDetails';
 import Login from '../components/Pages/Login';
 import Signup from "../components/Pages/Signup";
->>>>>>> Stashed changes
 
 function App() {
-    return(
-        <>
-<<<<<<< Updated upstream
-        <CreationWizard/>
-=======
-        <CommunityDetails/>
-        <Login/>
-        <Signup/>
->>>>>>> Stashed changes
-        </>
-
-    )
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />            {/* Login page */}
+        <Route path="/signup" element={<Signup />} />     {/* Signup page */}
+        <Route path="/create" element={<CreationWizard />} /> {/* Creation wizard */}
+        <Route path="/community/:id" element={<CommunityDetails />} /> {/* Community details */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
